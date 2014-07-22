3dfb38e98ad6ac6e9e5b336becab51309b9bf47e
Don Johnson <copyleftdev@gmail.com>
2014-07-21 21:33:08 -0700

    Added Connection Test

---
+++ src/connection_test.py
@@ -0,0 +1,15 @@
+import sys
+from pymongo import Connection
+from pymongo.errors import ConnectionFailure
+
+def main():
+	"""Connect to MongoDB"""
+	try:
+		c = Connection(host="dev", port=27017)
+		print "Connected successfully"
+	except ConnectionFailure as e:
+		sys.stderr.write("Could not connect to MongoDB: {0}".format(e))
+		sys.exit(1)
+
+if __name__ == '__main__':
+	main()
