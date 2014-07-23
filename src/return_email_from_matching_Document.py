b5671babcdb3048d770ac678f47019695ee369b9
Don Johnson <dj@codetestcode.io>
2014-07-22 21:29:42 -0700

    Return email from maching Documents
    Signed-off-by: Don Johnson <dj@codetestcode.io>

---
+++ src/return_email_from_matching_Document.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from pymongo import Connection
+from pymongo.errors import ConnectionFailure
+
+
+def main():
+	try:
+		c = Connection(host="dev",port=27017)
+	except ConnectionFailure, e:
+		sys.stderr.write('Could not connect to MongoDB: {}'.format(e))
+		sys.exit(1)
+	dbh = c["mydb"]
+
+	users = dbh.users.find({"firstname":"Douglas"}, {"email":1})
+	for user in users:
+		print(user.get("email"))
+
+if __name__ == '__main__':
+	main()
