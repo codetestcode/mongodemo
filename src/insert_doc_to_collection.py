2f58a3454de75010e575e260342addacf25490fa
Don Johnson <dj@codetestcode.io>
2014-07-22 21:28:47 -0700

    Refactored Mongo Insert Tool
    Added a Faker Data Generator Mechanizm
    
    Signed-off-by: Don Johnson <dj@codetestcode.io>

--- src/insert_doc_to_collection.py
+++ src/insert_doc_to_collection.py
@@ -6,6 +6,11 @@ import sys
 from datetime import datetime
 from pymongo import Connection
 from pymongo.errors import ConnectionFailure
+from faker import Factory
+import random
+
+
+fake = Factory.create()
 
 def main():
 	
@@ -16,18 +21,21 @@ def main():
 		sys.exit(1)
 	dbh = c['mydb']
 	assert dbh.connection == c
-
-	user_doc = {
-			"username": "codetestcode",
-			"firstname": "Donald",
-			"surname": "Johnson",
-			"dateofbirth": datetime(1975,4,12),
-			"email": "dj@codetestcode.io",
-			"score": 0
-	}
-
-	dbh.users.insert(user_doc, safe=True)
-	print("Successfully inserted document: {}".format(user_doc))
+	for i in range(0,300000):
+		user_doc = {
+			"username": "{}".format(fake.user_name()),
+			"firstname": "{}".format(fake.first_name()),
+			"surname": "{}".format(fake.last_name()),
+			"dateofbirth":"{}".format(fake.date(pattern="%Y-%m-%d")),
+			"email": "{}".format(fake.safe_email()),
+			"score": "{}".format(fake.random_int(min=5, max=100))}
+		
+		dbh.users.insert(user_doc,safe=True)
+		print(user_doc)
+	
+  #dbh.users.insert(user_doc, safe=True)
+  
+  
 
 if __name__ == '__main__':
 	main()
