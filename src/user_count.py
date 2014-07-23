7a8680b864e7d8d1d0f4f0e0e8e85eb8e41c7aa5
Don Johnson <dj@codetestcode.io>
2014-07-22 21:29:57 -0700

    Added User Count Module
    Signed-off-by: Don Johnson <dj@codetestcode.io>

---
+++ src/user_count.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from pymongo import Connection
+from pymongo.errors import ConnectionFailure
+
+
+def main():
+	try:
+		c = Connection(host="dev",port=27017)
+	except ConnectionFailure, e:
+		sys.stderr.write('Could not connect to MongoDB: {}'.format(e))
+		sys.exit(1)
+	dbh = c["mydb"]
+	usercount = dbh.users.find().count()
+	print(usercount)
+
+if __name__ == '__main__':
+	main()
